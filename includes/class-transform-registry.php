a04ef1256178d1b76ee2b2fbac410bd1c1037334
Chris Huber <chubes@extrachill.com>
2026-05-01 13:03:50 -0400

    fix: convert quote attribution wrappers (#97)

--- includes/class-transform-registry.php
+++ includes/class-transform-registry.php
@@ -1870,6 +1870,10 @@ class HTML_To_Blocks_Transform_Registry {
 			return false;
 		}
 
+		if ( self::class_matches( $element, '/(?:^|[-_\s])quote[-_\s]+(?:attr|attribution)(?:$|[-_\s])/i' ) ) {
+			return true;
+		}
+
 		if ( self::class_matches( $element, '/(?:^|[-_\s])(group|section|container|wrapper|wrap|content|main|article|aside|header|footer|inner|row|grid|card|product|compare|feature|visual|pin|location|detail|chrome|scroll|thumb|stars?|rating|info)(?:$|[-_\s])/i' ) ) {
 			return true;
 		}
